d154de97935ef762ae8c241082356e22459ea71b
G S S KARTHEEK <kartheek@sails.solutions>
2024-09-05 18:25:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-resource "azurerm_storage_account" "asa" {
-  name                     = var.resource_group_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
+# resource "azurerm_storage_account" "asa" {
+#   name                     = var.storage_account_name
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    environment = "staging"
-  }
-}
-# Create an Azure Storage Container
-resource "azurerm_storage_container" "asc" {
-  name                  = "skipper-container"
-  storage_account_name  = azurerm_storage_account.asa.name
-  container_access_type = "private"
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
+# # Create an Azure Storage Container
+# resource "azurerm_storage_container" "asc" {
+#   name                  = "skipper-container"
+#   storage_account_name  = azurerm_storage_account.asa.name
+#   container_access_type = "private"
+# }
 
-# Upload a sample file to the Azure Storage Container
-resource "azurerm_storage_blob" "sample_file_1" {
-  name                   = "sample-file-1.txt"
-  storage_account_name   = azurerm_storage_account.asa.name
-  storage_container_name = azurerm_storage_container.asc.name
-  type                   = "Block"
-  source                 = "files/sample-file-1.txt"
-}
+# # Upload a sample file to the Azure Storage Container
+# resource "azurerm_storage_blob" "sample_file_1" {
+#   name                   = "sample-file-1.txt"
+#   storage_account_name   = azurerm_storage_account.asa.name
+#   storage_container_name = azurerm_storage_container.asc.name
+#   type                   = "Block"
+#   source                 = "files/sample-file-1.txt"
+# }
 
-resource "azurerm_storage_blob" "sample_file_2" {
-  name                   = "sample-file-2.txt"
-  storage_account_name   = azurerm_storage_account.asa.name
-  storage_container_name = azurerm_storage_container.asc.name
-  type                   = "Block"
-  source                 = "files/sample-file-2.txt"
-}
+# resource "azurerm_storage_blob" "sample_file_2" {
+#   name                   = "sample-file-2.txt"
+#   storage_account_name   = azurerm_storage_account.asa.name
+#   storage_container_name = azurerm_storage_container.asc.name
+#   type                   = "Block"
+#   source                 = "files/sample-file-2.txt"
+# }
