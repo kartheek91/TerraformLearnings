0c17a3b4342d29fa1f25e22eeb40dd14a1e92008
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-21 16:59:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
+# Generate a random string for resource names
 resource "random_string" "storage_name" {
   length  = 8
   special = false    # Avoid special characters for compatibility
   upper   = false    # Azure resource names are case-insensitive
 }
 
+# Create an Azure Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "sails${random_string.storage_name.result}"
   location = var.location
 }
+
+# Create an Azure Storage Account
 resource "azurerm_storage_account" "asa" {
   name                     = "sails${random_string.storage_name.result}"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -15,13 +19,17 @@ resource "azurerm_storage_account" "asa" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
+  # The `min_tls_version` attribute is required for the storage account in some configurations
+  min_tls_version          = "TLS1_2" 
+
   tags = {
     environment = "staging"
   }
 }
+
 # Create an Azure Storage Container
 resource "azurerm_storage_container" "asc" {
-  name                  ="sails${random_string.storage_name.result}"
+  name                  = "sails${random_string.storage_name.result}"
   storage_account_name  = azurerm_storage_account.asa.name
   container_access_type = "private"
 }
