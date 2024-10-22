3a7e4752a2ae8e3d2d104c526175776fb90e7476
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-22 18:22:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "random_string" "storage_name" {
-  length  = 8
-  special = false    
-  upper   = false  
-}
+# resource "random_string" "storage_name" {
+#   length  = 8
+#   special = false    
+#   upper   = false  
+# }
 
 # resource "azurerm_resource_group" "rg" {
 #   name     = "sails${random_string.storage_name.result}"
@@ -12,11 +12,11 @@ module "resourcegroup" {
   source  = "app.terraform.io/kartheek91/resourcegroup/azure"
   version = "0.0.3"
   # insert required variables here
-  name = "sails${random_string.storage_name.result}"
+  name = "sailsskipper2024"
   location = var.location
 }
 resource "azurerm_storage_account" "asa" {
-  name                     = "sails${random_string.storage_name.result}"
+  name                     = "sailsskipper2024account"
   resource_group_name      = module.resourcegroup.name
   location                 = module.resourcegroup.location
   account_tier             = "Standard"
@@ -28,7 +28,7 @@ resource "azurerm_storage_account" "asa" {
 }
 # Create an Azure Storage Container
 resource "azurerm_storage_container" "asc" {
-  name                  = "skipper-${random_string.storage_name.result}"
+  name                  = "sailsskipper2024container"
   storage_account_name  = azurerm_storage_account.asa.name
   container_access_type = "private"
 }
@@ -40,12 +40,4 @@ resource "azurerm_storage_blob" "sample_file_1" {
   storage_container_name = azurerm_storage_container.asc.name
   type                   = "Block"
   source                 = "files/sample-file-11.txt"
-}
-
-resource "azurerm_storage_blob" "sample_file_2" {
-  name                   = "sample-file-2.txt"
-  storage_account_name   = azurerm_storage_account.asa.name
-  storage_container_name = azurerm_storage_container.asc.name
-  type                   = "Block"
-  source                 = "files/sample-file-2.txt"
 }
